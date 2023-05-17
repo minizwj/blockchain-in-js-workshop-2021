6e322f155ace141613b2a129e319b8501262b229
minizwj <3310407385@qq.com>
2023-05-17 22:20:53 +0800

    update

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,16 +1,26 @@
+
+import sha256 from 'crypto-js/sha256.js'
+export const DIFFICULTY = 2
+
 class Block {
   // 1. 完成构造函数及其参数
   /* 构造函数需要包含
-      - 名字
-      - 创世区块
-      - 存储区块的映射
+    区块链
+    上一个区块的哈希值
+    高度
+    时间戳
   */
-  constructor(name,prehash,index,sha256(new Date().getTime().toString()).toString()) {
-    this.name=name
-    this.prehash=prehash
-    this.index=index
-    this.hash=sha256(new Date().getTime().toString()).toString()
+  constructor(blockchain, previousHash, height, timestamp) {
+    this.blockchain = blockchain
+    this.previousHash = previousHash
+    this.height = height
+    this.timestamp = timestamp //输出时间戳
+    this.hash = this.calculateHash()
+
+  }
+
+  calculateHash () {
+    return sha256(this.previousHash, this.timestamp).toString()
   }
 }
-
 export default Block
--- src/models/Blockchain.js
+++ src/models/Blockchain.js
@@ -1,44 +1,37 @@
+
 // Blockchain
 class Blockchain {
   // 1. 完成构造函数及其参数
-  /* 构造函数需要包含 
+  /* 构造函数需要包含
       - 名字
       - 创世区块
       - 存储区块的映射
   */
   constructor(name) {
-  this.name=name
-  this.genesis=genesis
-  this.blocks=blocks// 后一个区块的hash，map结构
-}
+    this.name=name
+    this.genesis=null
+    this.blocks=new Map()
+  }
 
   // 2. 定义 longestChain 函数
-  /* 
+  /*
     返回当前链中最长的区块信息列表
   */
-  longestChain() {//将信息放到一个map中
-  var a1=new Map()
-  var a2=new Map()
-  //将各链上数据加到map中
-  //从头开始遍历区块
-  while (blockchain.blocks!=null){
-      if(genesis.blocks.size==1){//是一个map
-      a1.putAll(genesis.blocks)
-      a2.putAll(genesis.blocks)
-      }else{//遍历blocks集合里面的值
-         a1.set(genesis.blocks[0])
-         a2.set(genesis.blocks[1])
+    longestChain () {
+      let longestChain = [];
+      for (const [hash, block] of this.blocks) { //解构赋值
+        const chain = [block];
+        let previousBlock = block;
+        while (this.blocks.has(previousBlock.previousHash) && (this.blocks.has(previousBlock.previousHash)).has !== 'root') {
+          previousBlock = this.blocks.get(previousBlock.previousHash);
+          chain.unshift(previousBlock);
+        }
+        if (chain.length > longestChain.length) {
+          longestChain = chain;
+        }
       }
-      genesis=genesis.blocks
+      return longestChain;
+    }
   }
-  //选择加谁
-  if (a1.size>a2.size){
-  return a1
-  }else{
-  return  a2
-  }
-
-  }
-}
 
 export default Blockchain
